8bae83dd3f713b3a3910741bc26ddfbefafda91d
Paulinha Merlim <104771318+PaulaMerlim@users.noreply.github.com>
2022-08-03 15:12:14 -0300

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <h1>Obrigada Sté!</h1>
+        <h2> Alfabetizadora de códigos!</h2>
+      </>
+    );
+  }
+}
